a429af706f80c859be2a28909b48ff47cfefff9a
kenneth irungu <kennethirungu1800@gmail.com>
2020-07-10 13:58:10 +0300

    Updates

---
+++ precache-manifest.2787e1fd452fe4308c622e30e4b366df.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7d6a8d43f9e082319a649b01ad70922",
+    "url": "/Estimate/index.html"
+  },
+  {
+    "revision": "7964a8a91cc262c125e4",
+    "url": "/Estimate/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "3f2c23ffb8d8f3252d04",
+    "url": "/Estimate/static/css/main.14aaa9f2.chunk.css"
+  },
+  {
+    "revision": "7964a8a91cc262c125e4",
+    "url": "/Estimate/static/js/2.697aa06a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Estimate/static/js/2.697aa06a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f2c23ffb8d8f3252d04",
+    "url": "/Estimate/static/js/main.33229dc5.chunk.js"
+  },
+  {
+    "revision": "8bb770908f697f5b19b2",
+    "url": "/Estimate/static/js/runtime-main.6eb880ea.js"
+  }
+]);
